e0df00329e3d4f714a283d2a5942523587cc56f9
Vitaly Shlyaga <storkvist@storkvist.net>
2015-04-20 13:51:29 +0300

    Сохранение промежуточных данных между деплоями.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, '/home/user/webapps/avtomatikasm.ru'
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
